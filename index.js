709fb5e8ff1a98e5518f6385315bf3e819bade5e
Owen Lenihan <owenlenihan101@gmail.com>
2023-05-04 15:43:52 +0100

    Created basic file structure layout

---
+++ index.js
@@ -0,0 +1,12 @@
+let playerVictory = 5;
+let robotVictory = 5;
+let round = 0;
+
+function rockPaperScissors() {
+    console.log("Test")
+    console.log(playerVictory)
+    console.log(robotVictory)
+    console.log(round)
+}
+
+rockPaperScissors()

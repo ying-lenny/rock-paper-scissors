c83ff1a6b93a880e86274a997a4f7cdef09adac7
Owen Lenihan <owenlenihan101@gmail.com>
2023-05-04 22:43:45 +0100

    Added basic functionality for RPS

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
-let playerVictory = 5;
-let robotVictory = 5;
+let playerLives = 5;
+let robotLives = 5;
 let round = 0;
+let playerResult
+let robotPlay
+
+function countRounds() {
+    round += 1;
+    return round;
+}
+
+function attackTime(playerResult, robotPlay) {
+    if (playerResult === robotPlay) {
+        console.log("It's a draw!")
+    } else {
+        console.log("Wha?")
+    }
+}
 
 function rockPaperScissors() {
-    console.log("Test")
-    console.log(playerVictory)
-    console.log(robotVictory)
-    console.log(round)
+    const robotChoices = ["rock","paper","scissors"]
+    let robotPlay = robotChoices[Math.floor(Math.random()*robotChoices.length)];
+    let playerChoice = prompt("Okay Player, type your weapon of choice")
+    
+    console.clear()
+    console.log(playerLives)
+    console.log(robotLives)
+    console.log(playerResult)
+    console.log(robotPlay)
+    console.log(`The robot has chosen ${robotPlay}`)
+    
+    switch (playerChoice) {
+        case "Paper": case "P": case "paper": case "p":    
+            console.log("You have chosen Paper");
+            playerResult = "paper";
+            break;
+        case "Scissors": case "S": case "scissors": case "s":
+            console.log("You have selected Scissors");
+            playerResult = "scissors";
+            break;
+        case "Rock": case "R": case "rock": case "r":
+            console.log("You have picked Rock");
+            playerResult = "rock";
+            break;
+        case null:
+            console.log("Another time maybe...")
+            return
+        default:
+            console.log("Why?")
+            break;
+    }
+    attackTime(playerResult, robotPlay)
+    countRounds()
+    console.log(`Round: ${round}`)
+    rockPaperScissors()
 }
 
 rockPaperScissors()

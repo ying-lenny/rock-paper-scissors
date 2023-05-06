5b2c03246a0071786457fb11b380ce0b29fc32a2
Owen Lenihan <owenlenihan101@gmail.com>
2023-05-06 16:45:27 +0100

    Updated index.js to take in feedback

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let round = 0;
 let playerResult;
 let robotPlay;
 let finished = false;
+let lost;
+let playerChoice
 
 // Increments round counter by 1
 function countRounds() {
@@ -17,6 +19,10 @@ function startGame() {
     robotLives = 5;
     round = 0;
     rockPaperScissors()
+}
+
+function endGame() {
+    console.log("Another time maybe...")
 }
 
 // Attack phase of game logic
@@ -67,7 +73,7 @@ function isFinished() {
 
 // Decrements the lives of the round's loser
 function loseRound(loser) {
-    var lost = loser - 1;
+    lost = loser - 1;
     return lost;
 }
 
@@ -78,20 +84,13 @@ function explainGame() {
     "\nType help to see these instructions again")  
 }
 
-explainGame()
-
-// Game logic
-function rockPaperScissors() {
-    // Randomly determines robot's next move from preterminded list
-    const robotChoices = ["Rock","Paper","Scissors"]
-    let robotPlay = robotChoices[Math.floor(Math.random()*robotChoices.length)];
-    let playerChoice = prompt("Okay Player, type your weapon of choice")
+function playerTurn() {
+    playerChoice = prompt("Okay Player, type your weapon of choice")
     // Needed to catch when player hits cancel on prompt as trying to lowercase a null returns an error
     if (playerChoice != null) {
         playerChoice = playerChoice.replace(/ /g,'').toLowerCase()
     }
-    // Clears the console at the start of the next round
-    console.clear()
+
     switch (playerChoice) {
         case "paper": case "p": case "1":
             playerResult = "Paper";
@@ -103,8 +102,9 @@ function rockPaperScissors() {
             playerResult = "Rock";
             break;
         case null:
-            console.log("Another time maybe...")
-            return
+            console.clear()
+            endGame()
+            return;
         case "reset": 
             startGame();
             break;
@@ -117,23 +117,42 @@ function rockPaperScissors() {
         default:
             console.log("The player has chosen... something invalid?")
             playerResult = ""
+            alert("Sorry that was an incorret move buddy.")
             break;
     }
+    // Clears the console at the start of the next round
+    console.clear()
+}
 
-    console.log(`%cThe Player has chosen: ${playerResult} | The Robot has chosen: ${robotPlay}`,
+function robotTurn() {
+    // Randomly determines robot's next move from preterminded list
+    const robotChoices = ["Rock","Paper","Scissors"]
+    robotPlay = robotChoices[Math.floor(Math.random()*robotChoices.length)];
+}
+
+explainGame()
+
+// Game logic
+function rockPaperScissors() {
+    playerTurn();
+    robotTurn();
+
+    if (playerChoice != null) {
+        console.log(`%cThe Player has chosen: ${playerResult} | The Robot has chosen: ${robotPlay}`,
         "display: inline-block; background-color: darkblue; color: white; font-weight: 800;" + "padding: 10px;")   
-    
-    attackTime(playerResult, robotPlay)
-    if (playerResult != "") {
-        countRounds()
-    }
-    console.log(`%cPlayer lives: ${playerLives} | Robot Lives: ${robotLives}`,
-        "display: inline-block; background-color: darkblue; color: white; font-weight: 800;" + "padding: 10px;")
+        
+        attackTime(playerResult, robotPlay)
+        if (playerResult != "") {
+            countRounds()
+        }
+        console.log(`%cPlayer lives: ${playerLives} | Robot Lives: ${robotLives}`,
+            "display: inline-block; background-color: darkblue; color: white; font-weight: 800;" + "padding: 10px;")
 
-    console.log(`%cRound: ${round}`,
-        "display: inline-block; border: 3px solid red ; border-radius: 7px ; " +
-        "padding: 10px ; margin: 10px ;")
-    isFinished()
+        console.log(`%cRound: ${round}`,
+            "display: inline-block; border: 3px solid red ; border-radius: 7px ; " +
+            "padding: 10px ; margin: 10px ;")
+        isFinished()
+    } 
 }
 
 rockPaperScissors()
